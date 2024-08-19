30d494cdef5c2bfe374af96e31467c525f58791f
Darnel Castor <darnelcastor2@gmail.com>
2024-08-19 14:44:59 -0400

    Hompage Hero Section + Navbar pt1

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -3,19 +3,32 @@ import Image from "next/image";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { useUser, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 function Header() {
   const { user, isSignedIn } = useUser();
   return (
-    <div className="p-5 flex justify-center items-center border shadow-sm">
+    <div className="p-5 flex justify-between items-center border shadow-sm">
       <div className="flex flex-row items-center">
         {/* logo */}
-        <Image />
+        <Image src={"/chart-donut.svg"} alt="logo" width={40} height={25} />
+        <span className="text-blue-800 font-bold text-xl">FinanSmart</span>
       </div>
-      <div>
-        <span>FinanSmart</span>
-      </div>
-      <div>{/* userbutton */}</div>
+
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <div className="flex gap-3 items-center">
+          <Link href="/dashboard">
+            <Button variant="outline" className="rounded-full">
+              Dashboard
+            </Button>
+          </Link>
+          <Link href="/dashboard">
+            <Button>Get Started</Button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
-import { Inter } from "next/font/google";
+import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
-import { ClerkLoading } from "@clerk/nextjs";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
-const inter = Inter({ subsets: ["latin"] });
+const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
   title: "FinanSmart",
@@ -14,7 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider publishableKey={process.env.NEXT_CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={outfit.className}>
+          <Toaster />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
