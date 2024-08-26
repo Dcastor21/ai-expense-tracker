39cc240a1a4f2f1901b67be5863e54f28a687842
Darnel Castor <darnelcastor2@gmail.com>
2024-08-26 08:06:26 -0400

    Dashboard Components

---
+++ src/app/(routes)/dashboard/_components/DashboardHeader.jsx
@@ -0,0 +1,14 @@
+import { UserButton } from "@clerk/nextjs";
+
+function DashboardHeader() {
+  return (
+    <div className="p-5 shadow-sm border-b flex justify-between">
+      <div></div>
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+}
+
+export default DashboardHeader;
---
+++ src/app/(routes)/dashboard/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+
+import React, { useState } from "react";
+import { UserButton } from "@clerk/nextjs";
+
+function page() {
+  return <div>page</div>;
+}
+
+export default page;
