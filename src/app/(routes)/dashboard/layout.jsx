66850076e68b7f1151648486e7b636369570ceb5
Darnel Castor <darnelcastor2@gmail.com>
2024-08-30 20:55:29 -0400

    Dashboard

--- src/app/(routes)/dashboard/layout.jsx
+++ src/app/(routes)/dashboard/layout.jsx
@@ -1,106 +1,43 @@
 "use client";
 import React, { useEffect } from "react";
-import Image from "next/image";
-import {
-  LayoutGrid,
-  PiggyBank,
-  ReceiptText,
-  ShieldCheck,
-  CircleDollarSign,
-  TrendingUp,
-  TrendingDownIcon,
-} from "lucide-react";
-import { UserButton } from "@clerk/nextjs";
-import { usePathname } from "next/navigation";
-import Link from "next/link";
-function SideNav() {
-  const menuList = [
-    {
-      id: 1,
-      name: "Dashboard",
-      icon: LayoutGrid,
-      path: "/dashboard",
-    },
-    {
-      id: 2,
-      name: "Incomes",
-      icon: CircleDollarSign,
-      path: "/dashboard/incomes",
-    },
-    {
-      id: 2,
-      name: "Budgets",
-      icon: PiggyBank,
-      path: "/dashboard/budgets",
-    },
-    {
-      id: 3,
-      name: "Expenses",
-      icon: ReceiptText,
-      path: "/dashboard/expenses",
-    },
-    // {
-    //   id: 2,
-    //   name: "Investments",
-    //   icon: TrendingUp,
-    //   path: "/dashboard/investments",
-    // },
-    // {
-    //   id: 2,
-    //   name: "Debts",
-    //   icon: TrendingDownIcon,
-    //   path: "/dashboard/debts",
-    // },
-    {
-      id: 4,
-      name: "Upgrade",
-      icon: ShieldCheck,
-      path: "/dashboard/upgrade",
-    },
-  ];
-  const path = usePathname();
+import SideNav from "./_components/SideNav";
+import DashboardHeader from "./_components/DashboardHeader";
+import { db } from "../../../../utils/dbConfig";
+import { Budgets } from "../../../../utils/schema";
+import { useUser } from "@clerk/nextjs";
+import { Fira_Sans_Extra_Condensed } from "next/font/google";
+import { EmailAddress } from "@clerk/nextjs/dist/types/server";
+
+function DashboardLayout({ children }) {
+  const user = useUser();
+  const router = useRouter();
 
   useEffect(() => {
-    console.log(path);
-  }, [path]);
+    user && checkUserBudgets();
+
+    return () => {
+      Second;
+    };
+  }, [user]);
+
+  const checkUserBudgets = async () => {
+    const result = await db.select
+      .from(Budgets)
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress));
+    if (result?.length === 0) {
+      router.replace("/dashboard/budgets/");
+    }
+  };
+
   return (
-    <div className="h-screen p-5 border shadow-sm">
-      {/* <Image src={'/logo.svg'}
-        alt='logo'
-        width={160}
-        height={100}
-        /> */}
-      <div className="flex flex-row items-center">
-        <Image src={"./chart-donut.svg"} alt="logo" width={40} height={25} />
-        <span className="text-blue-800 font-bold text-xl">FinanSmart</span>
+    <div>
+      <div className="fixed md:w-64 hidden md:block">
+        <SideNav />
       </div>
-      <div className="mt-5">
-        {menuList.map((menu, index) => (
-          <Link href={menu.path} key={index}>
-            <h2
-              className={`flex gap-2 items-center
-                    text-gray-500 font-medium
-                    mb-2
-                    p-4 cursor-pointer rounded-full
-                    hover:text-primary hover:bg-blue-100
-                    ${path == menu.path && "text-primary bg-blue-100"}
-                    `}
-            >
-              <menu.icon />
-              {menu.name}
-            </h2>
-          </Link>
-        ))}
-      </div>
-      <div
-        className="fixed bottom-10 p-5 flex gap-2
-            items-center"
-      >
-        <UserButton />
-        Profile
+      <div className="md:ml-64">
+        <DashboardHeader />
+        {children}
       </div>
     </div>
   );
 }
-
-export default SideNav;
